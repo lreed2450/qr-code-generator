1d975e73aee6cb8b62e28c0155b7855c9a88fd50
lreed2450 <74885836+lreed2450@users.noreply.github.com>
2024-09-25 20:58:18 -0500

    updates

--- qr-code-generator.py
+++ qr-code-generator.py
@@ -7,11 +7,14 @@ st.header(body='QR Code Generator', divider='blue')
 
 st.write("Paste your url in the below textbox to generator the QR code")
 
-url = st.text_input(label='paste a url into the text box', placeholder='https://www.my-sample-url.com')
+st_url = st.text_input(label='paste a url into the text box', placeholder='https://www.my-sample-url.com')
 
-url_gen = pyqrcode.create(url)
+url_gen = pyqrcode.create(st_url)
 
 st.write("the image is below.")
 
+# url = pyqrcode.create(url_gen) 
 
-# st.image('uca.svg')
+# url.svg('test_qr.svg', scale=4, background="white", module_color="#7D007D")
+
+# st.image('test_qr.svg')
